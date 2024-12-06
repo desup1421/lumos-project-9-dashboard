866c9c556e4492b4a03cacb07b45f7442a8447b8
Dede Supriatna <dedesupriatna1421@mail.com>
2024-12-06 11:27:33 +0700

    feat: add feature to create new blog and portfolio

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getBlog } from "../redux/slices/blogSlice";
+import { useNavigate } from "react-router-dom";
 
 const Blogs = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { blogs, isLoading } = useSelector((state) => state.blog);
+  console.log(blogs);
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+    const formatter = new Intl.DateTimeFormat("id-ID", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    return formatter.format(newDate);
+  };
+
+  useEffect(() => {
+    dispatch(getBlog(1));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <h1>Blogs</h1>
@@ -15,12 +44,15 @@ const Blogs = () => {
               <th
                 scope="col"
                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase border-b-2"
-                colSpan="5"
+                colSpan="4"
               >
                 {/* Button Add */}
-                <button className="p-2 bg-primary-500 text-black rounded-sm float-right">
+                <button
+                  onClick={() => navigate("/blog/create")}
+                  className="p-2 bg-primary-500 text-black rounded-sm float-right"
+                >
                   <i className="bx bx-add-to-queue mr-1"></i>
-                  Add new member
+                  Add new artticle
                 </button>
               </th>
             </tr>
@@ -30,25 +62,13 @@ const Blogs = () => {
                 scope="col"
                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
               >
-                Photo
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-              >
-                Name
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-              >
                 Title
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+              >
+                Create Date
               </th>
               <th
                 scope="col"
@@ -59,36 +79,28 @@ const Blogs = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 group">
-            <tr className=" last:border-b">
-              <td className="px-6 py-4 text-sm font-medium text-black">
-                <div className="w-14 h-14 rounded-full overflow-hidden">
-                  <img
-                    src="https://picsum.photos/100"
-                    className="w-full h-full object-cover"
-                    alt=""
-                  />
-                </div>
-              </td>
-              <td className="px-6 py-4 text-sm text-black">dede@email.com</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-black">
-                Dede Supriatna
-              </td>
-              <td className="px-6 py-4 text-sm text-black">Programmer</td>
-              <td className="px-6 py-4 flex gap-1 justify-center items-center text-end text-sm font-medium">
-                {/* Button Delete */}
-                <button
-                  type="button"
-                  className="p-2 rounded-lg border border-red-500"
-                >
-                  <i className="bx bx-trash text-red-500"></i>
-                </button>
-              </td>
-            </tr>
+            {blogs?.data.map((item) => (
+              <tr key={item.id} className="last:border-b">
+                <td className="px-6 py-4 text-sm text-black">{item.title}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-black">
+                  {formatDate(item.created_at)}
+                </td>
+                <td className="px-6 py-4 flex gap-1 justify-center items-center text-end text-sm font-medium">
+                  {/* Button Delete */}
+                  <button
+                    type="button"
+                    className="p-2 rounded-lg border border-red-500"
+                  >
+                    <i className="bx bx-trash text-red-500"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getPortfolio } from "../redux/slices/portfolioSlice";
+import { useNavigate } from "react-router-dom";
 
 const Portfolio = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { portfolio, isLoading } = useSelector((state) => state.portfolio);
+
+  const formatDate = (date) => {
+    const newDate = new Date(date);
+    const formatter = new Intl.DateTimeFormat("id-ID", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    return formatter.format(newDate);
+  };
+
+  useEffect(() => {
+    dispatch(getPortfolio(1));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <h1>Portfolio</h1>
@@ -15,12 +43,15 @@ const Portfolio = () => {
               <th
                 scope="col"
                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase border-b-2"
-                colSpan="5"
+                colSpan="4"
               >
                 {/* Button Add */}
-                <button className="p-2 bg-primary-500 text-black rounded-sm float-right">
+                <button
+                  onClick={() => navigate("/portfolio/create")}
+                  className="p-2 bg-primary-500 text-black rounded-sm float-right"
+                >
                   <i className="bx bx-add-to-queue mr-1"></i>
-                  Add new member
+                  Add new project
                 </button>
               </th>
             </tr>
@@ -30,25 +61,19 @@ const Portfolio = () => {
                 scope="col"
                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
               >
-                Photo
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-              >
-                Name
+                Banner
               </th>
               <th
                 scope="col"
                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
               >
                 Title
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+              >
+                Create Date
               </th>
               <th
                 scope="col"
@@ -59,36 +84,37 @@ const Portfolio = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 group">
-            <tr className=" last:border-b">
-              <td className="px-6 py-4 text-sm font-medium text-black">
-                <div className="w-14 h-14 rounded-full overflow-hidden">
-                  <img
-                    src="https://picsum.photos/100"
-                    className="w-full h-full object-cover"
-                    alt=""
-                  />
-                </div>
-              </td>
-              <td className="px-6 py-4 text-sm text-black">dede@email.com</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-black">
-                Dede Supriatna
-              </td>
-              <td className="px-6 py-4 text-sm text-black">Programmer</td>
-              <td className="px-6 py-4 flex gap-1 justify-center items-center text-end text-sm font-medium">
-                {/* Button Delete */}
-                <button
-                  type="button"
-                  className="p-2 rounded-lg border border-red-500"
-                >
-                  <i className="bx bx-trash text-red-500"></i>
-                </button>
-              </td>
-            </tr>
+            {portfolio?.data.map((item) => (
+              <tr key={item.id} className="last:border-b">
+                <td className="px-6 py-4 text-sm font-medium text-black">
+                  <div className="w-14 h-14  overflow-hidden">
+                    <img
+                      src={item.banner}
+                      className="w-full h-full object-cover"
+                      alt={item.title}
+                    />
+                  </div>
+                </td>
+                <td className="px-6 py-4 text-sm text-black">{item.title}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-black">
+                  {formatDate(item.created_at)}
+                </td>
+                <td className="px-6 py-4 flex gap-1 justify-center items-center text-end text-sm font-medium">
+                  {/* Button Delete */}
+                  <button
+                    type="button"
+                    className="p-2 rounded-lg border border-red-500"
+                  >
+                    <i className="bx bx-trash text-red-500"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
